d331ea91cede11b69e73a012f2d8b1a7779e8650
Ofek Gila <flamingo.ofek@gmail.com>
2015-12-30 23:58:10 -0800

    sequence animation

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ $(document).ready(function() {
   $('.dropdown').animate({'top': "0px"}, "slow", function() {
     $('body').css('margin-top', $('.dropdown').outerHeight(true) + "px");
     $('.dropright').animate({left: $('.dropdown').outerWidth(true) + "px", opacity: 1}, 1000, function() {
+      after = true;
       $('#games').animate({opacity: 1, 'margin-top': "0px"}, 1000);
+      if($(document).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= $('#tools').position().top)
+        $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
     });
   });
 });
 
+var after = false;
 $(document).scroll(function() {
-  if($(this).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= $('#tools').position().top)
+  if(after && $(this).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= $('#tools').position().top)
     $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
 });
