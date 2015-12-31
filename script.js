ceb69b935823360be30295db9c44dd91c4d18de6
Ofek Gila <flamingo.ofek@gmail.com>
2015-12-30 18:21:26 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('body').css('margin-bottom', $('.footer').outerHeight(true) + "px");
+  $('.dropdown').css('margin-top', "-" + $('.dropdown').outerHeight(false) + "px");
+  $('.projects img').each(function() {
+    $(this).css('height', $(this).outerWidth(false) + "px");
+  });
+  $('.dropdown').animate({'margin-top': "0px"}, "slow", function() {
+    $('#games').animate({opacity: 1, 'margin-top': "0px"}, 1000, function() {
+      $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
+    });
+  });
+});
