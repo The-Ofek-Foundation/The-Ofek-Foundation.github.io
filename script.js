8426542759c7f423748bfced6c36acd87c33db42
Ofek Gila <flamingo.ofek@gmail.com>
2015-12-31 15:02:07 -0800

    add About Me

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $('body').css('margin-bottom', 2 * $('.footer').outerHeight(true) + "px");
   $('.dropdown').css('top', "-" + $('.dropdown').outerHeight(false) + "px");
-  $('.projects img').each(function() {
+  $('.info').css('margin-left', "-=25px").css('padding-left', "+=20px").css('padding-right', "+=20px").css('width', "-=40px");
+  $('.projects img, .info, img').each(function() {
     $(this).css('height', $(this).outerWidth(false) + "px");
   });
   $('.dropdown').animate({'top': "0px"}, "slow", function() {
@@ -9,14 +10,38 @@ $(document).ready(function() {
     $('.dropright').animate({left: $('.dropdown').outerWidth(true) + "px", opacity: 1}, 1000, function() {
       after = true;
       $('#games').animate({opacity: 1, 'margin-top': "0px"}, 1000);
-      if($(document).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= $('#tools').position().top)
-        $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
+      $(document).scroll();
     });
   });
 });
 
+// Animate tools when scrolled to them
 var after = false;
 $(document).scroll(function() {
-  if(after && $(this).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= $('#tools').position().top)
-    $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
+  if(after) {
+    if (window_at($('#tools')) && !$('#tools').data('animated')) {
+      $('#tools').animate({opacity: 1, 'margin-top': "0px"}, 1000);
+      $('#tools').data('animated', true);
+    }
+    if (window_at($('#about')) && !$('#about').data('animated')) {
+      $('#about').animate({opacity: 1, 'margin-left': "+=25px"}, 1000);
+      $('#about').css('height', 'auto');
+      $('#about').data('animated', true);
+    }
+    if (window_at($('#contact-me')) && !$('#contact-me').data('animated')) {
+      $('#contact-me').animate({opacity: 1, 'margin-left': "+=25px"}, 1000);
+      $('#contact-me').css('height', 'auto');
+      $('#contact-me').data('animated', true);
+    }
+  }
+});
+
+function window_at(elem) {
+  return $(document).scrollTop() + $(window).height() - $('.footer').outerHeight(true) >= elem.position().top;
+}
+
+// Scroll to top on window reload so always show up on top
+$(window).on('beforeunload', function() {
+  $('*').hide();
+  $(window).scrollTop(0);
 });
